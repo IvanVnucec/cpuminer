4034b0fba05ded006f9c7b100bd3fa64e67e5500
IvanVnucec <vnucec.ivan@gmail.com>
2021-08-27 15:08:37 +0200

    test sha

--- test/test_sha.c
+++ test/test_sha.c
@@ -1,3 +1,17 @@
+#include "miner.h"
+
+
 int main(void) {
+    int thr_id;
+    uint32_t *pdata;
+    const uint32_t *ptarget;
+	uint32_t max_nonce;
+    unsigned long hashes_done;
+    int rc;
+
+    rc = scanhash_sha256d(thr_id, pdata, ptarget, max_nonce, &hashes_done);
+    
+    (void) rc;
+
     return 0;
 }
